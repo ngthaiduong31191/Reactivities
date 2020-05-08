85d5e7331ad6a2fd2956c570f2d6ce26f0212417
duong <ngthaiduong31191@gmail.com>
2020-05-08 22:25:17 +0700

    "add create-react-app"

---
+++ client-app/src/CarItem.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { ICar } from './demo';
+
+interface IProps {
+    car: ICar
+}
+const CarItem: React.FC<IProps> = (props) => {
+    return (
+        <div>
+            <h1>{props.car.color}</h1>
+        </div>
+    )
+}
+export default CarItem;
